78f106b8aeaa49bad1b2b93034e160419c8b2412
ashif ali ansari <ashifaliansari07@gmail.com>
2026-01-05 18:09:34 +0530

    remove extra space

--- app/src/main/java/com/example/quizapp/CategoryActivity.java
+++ app/src/main/java/com/example/quizapp/CategoryActivity.java
@@ -23,7 +23,6 @@ public class CategoryActivity extends AppCompatActivity {
         btnProgramming.setOnClickListener(v -> startQuiz("programming", playerName));
     }
 
-
     private void startQuiz(String category, String playerName) {
         Intent intent = new Intent(CategoryActivity.this, QuizActivity.class);
         intent.putExtra("category", category);
